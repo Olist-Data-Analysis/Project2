4ed2352eda1c95466c7e5d097f3224908ec74cca
cye2020 <ariaplay101@gmail.com>
2025-10-22 21:24:02 +0900

    feat: 모델 및 유틸 추가

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,10 +1,10 @@
 from .chi_square_test import cramers_v, interpret_cramers_v, check_expected_frequencies, standardized_residuals
 from .one_way_anova import check_normality, check_homogeneity, calculate_eta_squared, calculate_epsilon_squared, perform_dunn_test, perform_gameshowell, perform_tukey_hsd
 from .calculate_distance import calculate_distance
-
+from .remove_outlier import detect_and_remove_outliers
 
 __all__ = [
     'cramers_v', 'interpret_cramers_v', 'check_expected_frequencies', 'standardized_residuals',
     'check_normality', 'check_homogeneity', 'calculate_eta_squared', 'calculate_epsilon_squared', 'perform_dunn_test', 'perform_gameshowell', 'perform_tukey_hsd',
-    'calculate_distance'
+    'calculate_distance', 'detect_and_remove_outliers'
 ]
